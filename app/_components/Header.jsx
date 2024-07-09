2742711803dc8d0a5664702a098a3fbf8dc96df5
Rizil PATEL <128392275+Rizil-Patel@users.noreply.github.com>
2024-07-09 15:59:36 +0530

    Update Header.jsx

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -13,12 +13,12 @@ function Header() {
             <Image src='/logo.svg' width={100} height={100} alt='logo'
                 className='w-[150px] md:w-[200px]'
             />
-            <ul className='hidden md:flex gap-14 font-medium text-lg'>
+{/*             <ul className='hidden md:flex gap-14 font-medium text-lg'>
                 <li className='hover:text-primary transition-all duration-300 cursor-pointer'>Product</li>
                 <li className='hover:text-primary transition-all duration-300 cursor-pointer'>Pricing</li>
                 <li className='hover:text-primary transition-all duration-300 cursor-pointer'>Contact us</li>
                 <li className='hover:text-primary transition-all duration-300 cursor-pointer'>About Us</li>
-            </ul>
+            </ul> */}
             <div className='flex gap-5'>
               <LoginLink> <Button variant="ghost">Login</Button></LoginLink> 
                <RegisterLink><Button>Get Started</Button></RegisterLink> 
